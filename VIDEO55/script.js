57569d5586df35ecf5e33a5590a54771b2d72d12
YogurtFusion <Aniketkumar33805@outlook.com>
2025-05-19 14:02:35 +0530

    1st  tutorial of JS completed

---
+++ VIDEO55/script.js
@@ -0,0 +1,60 @@
+console.log("cod is running");
+let a = 5;
+//  var a = 5;
+a = a + 1; // this is alllowed
+let b = 6; //var b = 6;
+let c = "mocha_loft"; //var c = "mocha_loft";
+
+{
+     let a = 66;
+     console.log(a);
+ }
+
+// {
+//    var a = 66;
+//     console.log(a);
+// }
+
+console.log(a);
+
+
+//let 55a = "rohan"// not valid Not Allowed;
+// var 55a = "rohan" not valid Not Allowed;
+
+// console.log(a + b);
+// console.log(a + b + c);
+// console.log(a + b + 8);
+// console.log(typeof a, typeof b, typeof c );
+
+// const a1 = 5;
+// a1 = a1 + 1;  // not allowed 
+
+
+let x = "Chapple Cheese";
+let y = "22";
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+let o = {
+    "name": "Harry",
+    "job code": 5600,
+    // "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o)
+o.is_handsome = "true";
+console.log(o)
+
+
+// let Y = "check"
+// console.log (Y)
