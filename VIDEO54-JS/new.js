eeaa7d5000a30c222bae9f762074c890465c5bd8
YogurtFusion <Aniketkumar33805@outlook.com>
2025-05-19 10:55:24 +0530

    JS started

---
+++ VIDEO54-JS/new.js
@@ -0,0 +1,3 @@
+console.log("Hello World")
+console.log("code is also running....")
+console.log("code is looking like a WoW....")
---
+++ VIDEO54-JS/script.js
@@ -0,0 +1,19 @@
+alert("hello World")
+
+console.log("code is running....")
+console.log("code is also running....")
+console.log("code is looking like a WoW....")
+
+var a = prompt("Enter your no.")
+
+var isTrue = confirm("Are you sure you want to leave this page and blast you pc")
+
+if(isTrue){
+    console.log("your pc is blasting")
+}
+
+else{
+    console.log("computure is not blasting")
+
+}
+console.log("your no. is " + a)
