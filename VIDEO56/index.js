5a41359d0d2ea9e026752bf055a228acdcde21e1
YogurtFusion <Aniketkumar33805@outlook.com>
2025-05-20 11:34:13 +0530

    video56 completed

---
+++ VIDEO56/index.js
@@ -0,0 +1,109 @@
+// console.log("Calculating that you can drive or not");
+
+let age  = 0;
+// let grace = 2;
+// age += grace;
+
+// console.log(age);
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age / grace);
+// console.log(age * grace);
+// console.log(age **grace);
+// console.log(age %grace);
+
+// if ((age**grace)>18){
+//     console.log("you can drive");
+// }
+
+
+// else {
+//     console.log("you can't drive");
+// }  
+
+//Multiverst of = 
+
+let age0 = 18;
+let age1 = 2323;
+// if (age0==18){
+//     console.log("True")
+// }
+
+
+// else {
+//     console.log("nope")
+// }
+
+//only value
+// if (age1==18){
+//     console.log("True")
+// }
+
+// else {
+//     console.log("nope")
+// }
+
+// value + type
+
+// if (age0===18){
+//     console.log("True")
+// }
+
+// else{
+//     console.log("nope")
+// }
+// not equaly to
+// if (age0!=18){
+//     console.log("True")
+// }
+
+// else{
+//     console.log("nope")
+// }
+
+//logical opretors
+let age4 = 18
+
+if (age0==age4 && age0 == age4){
+    console.log("ture")
+}
+
+
+
+if (age != age4 || age0 == age){
+    console.log("true")
+}
+
+
+
+
+if(age!=16){
+    console.log("you can drive")
+}
+
+else if (age == 0){
+    console.log("are you kidding")
+}
+
+else {
+    console.log("you can't drive")
+}
+
+
+let a = 6;
+let b = 8 ;
+
+let c = a>b ? (a-b) : (b-a);
+
+console.log(c)
+
+
+// translates to :
+// if (a>b) {
+//     let c=a -b;
+
+// }
+
+// else {
+//     let c = a -b;
+// }
