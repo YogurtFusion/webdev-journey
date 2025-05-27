d42a6714c27cf5dfe87949c2f3a13dbad75833ea
YogurtFusion <Aniketkumar33805@outlook.com>
2025-05-27 23:06:07 +0530

    comming back

---
+++ VIDEO59/index.js
@@ -0,0 +1,3 @@
+//Faultu calculator
+
+// This does the following : 1. It takes   
---
+++ VIDEO60/index.js
@@ -0,0 +1,18 @@
+console.log("This is a string tutorial")
+let a = "Mocha";
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+// console.log(a[6])
+
+console.log(a.length)
+
+let real_name = "MOcha"
+let friend = "loft"
+
+console.log("His fav cafe name is " + real_name + " and his fav cafe last name is " + friend )
+
+console.log(`His fav cafe first  name is ` ${real_name} 
