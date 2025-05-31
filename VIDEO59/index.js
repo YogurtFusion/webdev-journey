2d57215d621322a641b2692d4beb4d5f0dc246c2
YogurtFusion <Aniketkumar33805@outlook.com>
2025-05-31 20:01:09 +0530

    ex 9 comp

--- VIDEO59/index.js
+++ VIDEO59/index.js
@@ -1,3 +1,22 @@
 //Faultu calculator
 
-// This does the following : 1. It takes   
+// This does the following : 
+// 1. It takes  two numbers as an input from the user 
+// 2. It performs the worng operation as follows:
+// + --> -
+// * --> +
+// - --> /
+// / --> ** 
+// 
+// It performs wrong operations only 10% of the time 
+// 
+// ;
+
+let a = Math.random()
+
+let b = Math.random()
+
+console.log ("a + b = " + (a-b))
+console.log ("a * b = " + (a+b))
+console.log ("a - b = " + (a/b))
+console.log ("a / b = " + (a**b))
---
+++ VIDEO61/index.js
@@ -0,0 +1,46 @@
+// console.log("This is a string tutorial")
+// let a = "Mocha";
+
+// console.log(a[0])
+// console.log(a[1])
+// console.log(a[2])
+// console.log(a[3])
+// console.log(a[4])
+// // console.log(a[6])
+
+// console.log(a.length)
+
+// let real_name = "MOcha"
+// let friend = "loft"
+
+// console.log("MY  fav cafe name is " + real_name + " and his fav cafe last name is " + friend )
+
+// console.log(`My fav cafe first  name is  ${real_name} and last name is ${friend}`) 
+
+// // this properties are not used genraly
+// console.log("Here I'm going to know about \n how backslash n going to work ")
+// console.log("Here I'm going to know about \" how backslash \" going to work  ")
+// console.log("Here I'm going to know about \t how backslash t going to work ")
+// console.log("Here I'm going to know about \r how backslash r going to work ")
+
+let random = Math.random()
+let a = prompt("Enter first no.")
+let c = prompt("Enter operation")
+let b = prompt("Enter your second no.")
+
+let obj = {
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**",
+}
+
+if (random>0.1) {
+    console.log(`THe result is ${a} ${c} ${b}`)
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+}
+
+else {
+    c = obj(c)
+    alert(`The result is ${eva(`${a} ${c} ${b}`)}`)
+}
