010ab46ace2a5930c7a42e72ecaa020696d0a6a2
YogurtFusion <Aniketkumar33805@outlook.com>
2025-06-15 20:35:06 +0530

    ex in js

---
+++ VIDEO56/script.js
@@ -0,0 +1,59 @@
+let a = 12;
+
+if (a>10 && a<20){
+    console.log("age  is between 10 and 20");
+}
+
+else {
+    console.log("age is greater then 20");
+}
+
+//2
+
+let no1 = (2);
+
+let no2 = (3);
+
+if (no1 == no2){
+    console.log("no.1 and no.2 2 are same")
+}
+
+else {
+    console.log("not equal")
+}
+
+
+//queston no.3
+
+let ab = 32
+
+if (ab%3==0 && ab%2==0){
+    console.log("yes the no. is divisibl by 2 and 3");
+}
+
+else {
+    console.log("no the no. is not divisible");
+}
+
+
+//Ques4
+
+if (ab%3==0 || ab%2==0){
+    console.log("yes the no. is divisible");
+}
+
+else{
+    console.log("not divisible");
+}
+
+//Ques 5
+
+let age = 10
+
+if (age>18){
+    console.log("yes you can drive")
+}
+
+else {
+    console.log("you can't drive")
+}
---
+++ VIDEO62/index.js
@@ -0,0 +1,44 @@
+// Random bussines name genereator 
+// 
+// Adjectives:
+// Crazy 
+// Amazing
+// Fire
+// 
+// Shop Name:
+// Engine
+// Foods
+// Garments
+// 
+// Another Word
+// Bros
+// Limited
+// Hub
+// 
+
+// Adjectives
+
+
+let Random1 = {
+ver1: "Crazy",
+ver2: "Amazing",
+ver3: "Fire",
+    
+}
+
+// shop name;
+let Random2 = {
+    
+    shop1: "Engine",
+    shop2: "Foods",
+     shop3: "Garments",
+}
+
+// Another Words;
+
+let word1 = "Bros";
+let word2 = "Limited";
+let word3 = "Hub";
+
+
+
