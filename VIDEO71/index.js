ae8bbef4c1d4d87edb8477356053d415dba7813f
YogurtFusion <Aniketkumar33805@outlook.com>
2025-07-04 00:48:04 +0530

    comp

--- VIDEO71/index.js
+++ VIDEO71/index.js
@@ -12,4 +12,45 @@ console.log(document .querySelector(".container").innerHTML);
 console.log(document .querySelector(".container").innerText);
 console.log(document .querySelector(".container").textContent);
 
-console.log(document.querySelector(".container").hidden = true);
+// console.log(document.querySelector(".container").hidden = true); // can hide any page
+
+console.log(document.querySelector(".box").hasAttribute("style"));
+
+console.log(document.querySelector(".box").getAttribute("style"));
+
+console.log(document.querySelector(".box") .setAttribute("style", "display: inline"));
+
+console.log(document .querySelector(".box") .attributes);
+
+console.log(document.querySelector(".box").removeAttribute("style"));
+
+console.log(document.designMode = "on");// can edit any website locally
+
+console.log(document.querySelector(".box").dataset);
+
+let div = document.createElement("div");
+div.innerHTML = "I have been inserted"
+div.setAttribute("class", "created")
+document.querySelector(".container").before(div)
+
+// explore append
+
+
+
+let cont = document.querySelector(".container")
+cont.insertAdjacentHTML("beforebegin","  <b> I'm under the water please help me iouuuuu...</b> ")
+
+
+console.log(document.querySelector(".container").classList);
+
+console.log(document.querySelector(".container").className);
+
+console.log(document.querySelector(".container").classList.add("mocha"));
+
+console.log(document.querySelector(".container").classList.remove("mocha"));
+
+console.log(document.querySelector(".container") .classList.add("add", "this"));
+
+console.log(document.querySelector(".container").classList.toggle("red"));
+// browser needs one and you have to use loop for multiple time see it
+console.log(document.querySelector(".container").classList.toggle("red"));
