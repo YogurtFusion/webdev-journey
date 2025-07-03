3ccd72a996072b72ccc8f5d9dccc044cbef37c64
YogurtFusion <Aniketkumar33805@outlook.com>
2025-07-04 00:00:57 +0530

    m

---
+++ VIDEO71/index.js
@@ -0,0 +1,15 @@
+console.log(document .querySelector(".box").innerHTML);
+console.log(document.querySelector(".box") .innerHTML= "I'm changed through html  selector");
+
+console.log(document .querySelector(".box").outerHTML);
+console.log(document.querySelector(".box").nodeName);
+console.log(document.querySelector(".box") .tagName);
+
+
+
+console.log(document .querySelector(".container").innerHTML);
+
+console.log(document .querySelector(".container").innerText);
+console.log(document .querySelector(".container").textContent);
+
+console.log(document.querySelector(".container").hidden = true);
