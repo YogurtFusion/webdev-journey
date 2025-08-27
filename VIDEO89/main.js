8d4d27b94e91020dc74395c6f95e5d4ff768f24b
YogurtFusion <aniketkumar33805@outlook.com>
2025-08-27 20:18:15 +0530

    fuck this shit

--- VIDEO89/main.js
+++ VIDEO89/main.js
@@ -2,34 +2,39 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static("public"))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/about', (req, res) => {
-  res.send('about us')
-})
-app.get('/contact', (req, res) => {
-  res.send('<h1>Hello you can contact me here</h1> <br> ph: 323322323')
-})
-app.get('/blog', (req, res) => {
-  res.send('Hello blog page')
-})
-// http://localhost:3000/blog/chapple?mode=dark&region=in
-app.get('/blog/:slug', (req, res) => {
-    console.log(req.params);
-    console.log("yeah pramas is working");
-    
-    console.log(req.query);
-    
-    
-    
-  res.send(`hello ${req.params.slug}`)
+    console.log("Hey it's a get request");
+
+    res.send('Hello World!2')
 })
 
+app.post('/', (req, res) => {
+    console.log("Hey it's a post request");
+    res.send('Hello World POST!')
+})
+app.put('/', (req, res) => {
+    console.log("Hey it's a put request");
+    res.send('Hello World PUT!')
+})
+
+// app.get('/index', (req, res) => {
+//     console.log("Hey it's index.html file");
+
+//     res.send('Hello World index!')
+// })
+
+app.get('/index', (req, res) => {
+    console.log("Hey it's index.html file");
+
+    res.sendFile('templates/index.index',{root: __dirname})
+})
+
+app.get("/api", (req, res)=>{
+    res.json({a:1, b:2, c:3, d:4, name: ["chapple", "cheese"]})
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
-console.log("Server started. Waiting for requests...");
