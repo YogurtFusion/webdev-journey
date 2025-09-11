22f9ed03d9a16139091a1805548048bd0101f33a
YogurtFusion <aniketkumar33805@outlook.com>
2025-09-11 17:04:18 +0530

    m

---
+++ VIDEO98-tailwind again/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // this matches index.html, index-style.html, etc
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
