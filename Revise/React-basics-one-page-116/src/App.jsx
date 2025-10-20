dd5943e91f7960d3b8e1d7c5bc50ce7e3f7c4b35
YogurtFusion <aniketkumar33805@outlook.com>
2025-10-21 00:15:22 +0530

    use state and effect learning

--- Revise/React-basics-one-page-116/src/App.jsx
+++ Revise/React-basics-one-page-116/src/App.jsx
@@ -5,79 +5,79 @@ function App() {
   const [count, setCount] = useState(0)
   const [name, setName] = useState("Enter Name")
   const [tName, setTname] = useState("Enter title  ")
-  const [fruits, setFruit] = useState(["Apple ", "Banana ", "Cheery" ])
+  const [fruits, setFruit] = useState(["Apple ", "Banana ", "Cheery"])
   const [addFruit, setAddFruit] = useState("")
 
 
   function newFruit() {
-    if(addFruit.trim()!==""){
+    if (addFruit.trim() !== "") {
       setFruit([...fruits, addFruit]);
       setAddFruit("")
     }
   }
 
-  function delteFruit(indexToDelete){
+  function delteFruit(indexToDelete) {
     setFruit(fruits.filter((_, index) => index !== indexToDelete))
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log("name changed to: ", name)
   }, [name])
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log("you chaned title name")
-  document.title = tName
+    document.title = tName
   }, [tName])
   return (
     <>
-    {/* SO USE state is just a variable for react if it changes then the value in react changes  */}
-    <div className='bg-yellow-400 flex justify-center flex-col items-center gap-8 text-white text-xl'>
-    <h2>Count: {count} </h2>
-    <div className='flex gap-4 '>
+      {/* SO USE state is just a variable for react if it changes then the value in react changes  */}
+      <div className='bg-yellow-400 flex justify-center flex-col items-center gap-8 text-white text-xl'>
+        <h2>Count: {count} </h2>
+        <div className='flex gap-4 '>
 
-    <button className=' border-2  border-solid border-white rounded-full px-4 py-1 my-2' onClick={()=>setCount(count+1)}>Increase</button>
-    <button className=' border-2  border-solid border-white rounded-full px-4 py-1 my-2' onClick={()=> setCount(count-1)}>Decrease</button>
-    <button className=' border-2  border-solid border-white rounded-full px-4 py-1 my-2' onClick={()=>setCount(count*0)}>Reset</button>
+          <button className=' border-2  border-solid border-white rounded-full px-4 py-1 my-2' onClick={() => setCount(count + 1)}>Increase</button>
+          <button className=' border-2  border-solid border-white rounded-full px-4 py-1 my-2' onClick={() => setCount(count - 1)}>Decrease</button>
+          <button className=' border-2  border-solid border-white rounded-full px-4 py-1 my-2' onClick={() => setCount(count * 0)}>Reset</button>
 
-    </div>
-    </div>
+        </div>
+      </div>
 
-{/* use Effect */}
-   <div className='my-3 mx-auto w-1/2 bg-[#EEE8AA] px-8 py-10   text-center'>
+      {/* use Effect */}
+      <div className='my-3 mx-auto w-1/2 bg-[#EEE8AA] px-8 py-10   text-center'>
 
-   <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
-   <p>Hello: {name} </p>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <p>Hello: {name} </p>
 
-<div>
-  <input type="text" value={tName} onChange={(e)=> setTname(e.target.value)} />
-  <p>Title namem: {tName} </p>
-</div>
+        <div>
+          <input type="text" value={tName} onChange={(e) => setTname(e.target.value)} />
+          <p>Title namem: {tName} </p>
+        </div>
 
-   </div>
+      </div>
 
-   {/* .map  */}
+      {/* .map  */}
 
-   <ul className='flex flex-col items-center justify-center gap-8 bg-amber-50 py-4 px-2'>
-    {
-      fruits.map((fruits,index) => (
-        
-        <>
-        <li className='flex gap-3 text-xl text-gray-700' key={index}> {fruits} 
-          <button className='text-xs font-bold text-black border-2 border-dotted py-2 px-3 rounded-full' onClick={()=> delteFruit(index)}>Delete</button>
-        </li>
-        </>
-      ))
-    }
+      <ul className='flex flex-col items-center justify-center gap-8 bg-amber-50 py-4 px-2'>
+        {
+          fruits.map((fruits, index) => (
 
-    <div className='flex justify-center items-center'>
+            <>
+              <li className='flex gap-3 text-xl text-gray-700' key={index}> {fruits}
+                <button className='text-xs font-bold text-black border-2 border-dotted py-2 px-3 rounded-full' onClick={() => delteFruit(index)}>Delete</button>
+              </li>
+            </>
+          ))
+        }
 
-        <input type="text" placeholder='Enter your fruit name' value={addFruit} onChange={(e) => setAddFruit(e.target.value)} />
+        <div className='flex justify-center items-center'>
 
-        <button className='text-xs font-bold text-black border-2 border-dotted py-2 px-3 rounded-full' onClick={newFruit} >Add fruit</button>
-    
-    </div>
-   </ul>
-   
+          <input type="text" placeholder='Enter your fruit name' value={addFruit} onChange={(e) => setAddFruit(e.target.value)} />
+
+          <button className='text-xs font-bold text-black border-2 border-dotted py-2 px-3 rounded-full' onClick={newFruit} >Add fruit</button>
+
+        </div>
+      </ul>
+
     </>
   )
 }
