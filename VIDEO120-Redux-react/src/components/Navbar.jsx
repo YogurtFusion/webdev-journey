c230a6e71535a934fea2de94518756aa90dd8e1b
YogurtFusion <aniketkumar33805@outlook.com>
2025-10-26 18:53:15 +0530

    redux

--- VIDEO120-Redux-react/src/components/Navbar.jsx
+++ VIDEO120-Redux-react/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
-function Navbar({count}) {
-  return (
-    <div>
-        I'm navbar and count is {count}      
-    </div>
-  )
+import { useSelector, useDispatch } from 'react-redux'
+function Navbar() {
+    const count = useSelector((state) => state.counter.value)
+    return (
+        <div>
+            I'm navbar and count is {count}
+        </div>
+    )
 }
 
 export default Navbar
