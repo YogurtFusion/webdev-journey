98a232d96a4093c11881b982475c3b9eb2e6f5a2
YogurtFusion <aniketkumar33805@outlook.com>
2025-10-27 22:57:53 +0530

    m

--- VIDEO121-NEXT-JS/first/app/about/page.js
+++ VIDEO121-NEXT-JS/first/app/about/page.js
@@ -3,7 +3,14 @@ import React from 'react'
 function about() {
   return (
     <div>
-        I'm a about page      
+      <h1 className="text-xl text-white"> problem sovlved by chapple chesse </h1>      
+
+      <ul>
+        <li>Full stack solution </li>
+        <li>file based Routing</li>
+        <li>Additional features like router frm nex/navigation </li>
+        <li>Optimize rendering</li>
+      </ul>
     </div>
   )
 }
