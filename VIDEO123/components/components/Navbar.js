62f0051018a9edff86ca74d86fb601e7193e4439
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-01 22:30:23 +0530

    m

---
+++ VIDEO123/components/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav className="flex justify-around bg-slate-800 text-white py-4">
+            <div className="logo font-bold">Facebook</div>
+            <ul className='flex gap-6' >
+                <a href="/"><li>Home</li></a>
+                <a href="/about"><li>About</li></a>
+                <a href="/contact"><li>Contact</li></a>
+            </ul>
+        </nav>      
+    </div>
+  )
+}
+
+export default Navbar
