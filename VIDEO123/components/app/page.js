64457d2bc29f2b1b189a2473c7fbf6dbddc0a455
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-02 00:43:58 +0530

    m

--- VIDEO123/components/app/page.js
+++ VIDEO123/components/app/page.js
@@ -3,8 +3,9 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page} container >
-    <img src="https://picsum.photos/500" alt="" />
+    <div className= {styles.page}>
+    <img src="https://picsum.photos/1500" width={500} height={500} alt="Random imgae" fill={true} object-contain />
+   <Image src= "https://picsum.photos/1000" width={500} height={500} alt="Random iamge" />
     </div>
   );
 }
