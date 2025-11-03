c8d4c908696ecb7e655a6dd19e711df4b6ea9a94
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-04 00:22:43 +0530

    m

--- VIDEO124/my-app/app/api/add/route.js
+++ VIDEO124/my-app/app/api/add/route.js
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
  
 export async function POST(request) {
-    return NextResponse.json({success: true, data: "yes"})
+    let data = await  request.json()
+    console.log(data);
+    return NextResponse.json({success: true, data})
+
 }
  
--- VIDEO124/my-app/app/page.js
+++ VIDEO124/my-app/app/page.js
@@ -1,66 +1,29 @@
-import Image from "next/image";
+"use client"
+import Imagse from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  
+  const handleClick = async  () => {
+    let data = {
+      name: "Harry",
+      role: "Coder"
+    }
+    let a = await fetch("/api/add", {
+      method: "POST", headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+    let res = await a.json()
+    console.log(res);
+    
+  }
+  
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+     <h1>Next js Api routes demo</h1>
+     <button onClick={handleClick} >click me </button>
     </div>
   );
 }
