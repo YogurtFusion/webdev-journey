26c0ad5f5b016adb5341ead52efda9a74e6757b3
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-06 00:12:42 +0530

    m

--- VIDEO125/server-actions/actions/form.js
+++ VIDEO125/server-actions/actions/form.js
@@ -1,7 +1,7 @@
+"use server"
 import fs from "fs/promises"
 
  export const SubmitAction = async (e) => {
-    "use server"
     console.log(e.get("name"), e.get("add"));
     let a = await fs.writeFile("chapple.txt", `name is ${e.get("name")} and address is ${e.get("add")}`)
     
