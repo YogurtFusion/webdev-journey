fc77d470083c8486ebef9bd1ba2a09f2409aea97
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-05 23:57:16 +0530

    m

--- VIDEO125/server-actions/app/page.js
+++ VIDEO125/server-actions/app/page.js
@@ -1,13 +1,33 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import fs from "fs/promises"
 
 export default function Home() {
+
+  const SubmitAction = async (e) => {
+    "use server"
+    console.log(e.get("name"), e.get("add"));
+    let a = await fs.writeFile("chapple.txt", "heyy it's from the chapple ")
+    
+  }
   return (
-    <form action="">
-      <div>
+  <div className={styles.page} >
+    <form action={SubmitAction} className={styles.main}>
+      <div >
         <label htmlFor="name">Name</label>
         <input type="text" name="name" id="name" className="text-white" />
       </div>
+      <div>
+        <label htmlFor="add">Address </label>
+        <input name="add" id="add"
+        className="text-white"  type="text" />
+      </div>
+
+      <div>
+        <button>Submit</button>
+        
+      </div>
     </form>
+    </div>
   );
 }
