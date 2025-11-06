aa43931f00b4b10a4404dcaebeef9886491ce6df
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-06 23:51:34 +0530

    m

---
+++ VIDEO126/middleware/app/gallery/page.js
@@ -0,0 +1,8 @@
+// app/about/page.jsx
+export default function AboutPage() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-50 p-8">
+      
+    </main>
+  );
+}
