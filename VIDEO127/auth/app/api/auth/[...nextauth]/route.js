0414fdb82767b1cec0bfaec83b83f239330d8216
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-08 20:58:20 +0530

    m

---
+++ VIDEO127/auth/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,36 @@
+// app/api/auth/[...nextauth]/route.js
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+const handler = NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+})
+
+export { handler as GET, handler as POST }
+
+
+// import NextAuth from "next-auth"
+// import GithubProvider from "next-auth/providers/github"
+
+
+// // Configure one or more authentication providers
+// const handler = NextAuth({
+
+//     providers: [
+//         GithubProvider({
+//             clientId: process.env.GITHUB_ID,
+//             clientSecret: process.env.GITHUB_SECRET,
+//         }),
+//         // ...add more providers here
+//     ],
+//     secret: process.env.NEXTAUTH_SECRET,
+// })
+
+
+// export { handler as Get, handler as POST }
