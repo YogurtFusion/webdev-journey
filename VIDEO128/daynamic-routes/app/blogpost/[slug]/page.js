9663d5f3f2f4028e4eded38cd01fc7cc5ce3ec35
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-09 20:23:36 +0530

    slug

--- VIDEO128/daynamic-routes/app/blogpost/[slug]/page.js
+++ VIDEO128/daynamic-routes/app/blogpost/[slug]/page.js
@@ -1,4 +1,11 @@
 export default async function Page({params}){
-    const {slug} = await params
-    return  <div>My post: {slug}</div>
+    const {slug: rawSlug} = await params
+    const slug = decodeURIComponent(rawSlug)
+    let lan = ["python", "JavaScript", "Java", "cpp", "cs", "c#"]// # = %23
+    if (lan.includes(slug)){
+        return  <div>My post: {slug}</div>
+    }
+    else {
+        return <div>Post not found 🍊🍊</div>
+    }
 }
