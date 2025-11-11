958018e071f9c0de12944109e622e581dd4a8df4
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-11 21:34:40 +0530

    push

---
+++ VIDEO129/layout-nextjs/app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='text-sm font-[poppins] text-center font-bold ' >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non?      
+    </div>
+  )
+}
+
+export default Footer
