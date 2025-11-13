89355d26bad573c19433cb5bdc5bffb6b44b3b77
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-13 23:43:04 +0530

    passop-passwordmanager

--- VIDEO130/passop/src/App.jsx
+++ VIDEO130/passop/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import Manager from './components/Manager'
+
 
 function App() {
 
@@ -9,6 +11,7 @@ function App() {
    <>
    <div className=''>
     <Navbar/>
+    <Manager/>
    </div>
    </>
   )
--- VIDEO130/passop/src/components/Navbar.jsx
+++ VIDEO130/passop/src/components/Navbar.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <nav className='bg-purple-200  ' >
-            <div className="flex justify-between items-center px-4 h-14 mycontainer">
+        <nav className='bg-slate-800  ' >
+            <div className="flex justify-between items-center px-4 py-5 h-14 mycontainer">
 
-                <div className='logo font-bold'> <a href="/">Passop</a> </div>
+                <div className='logo font-bold text-white text-2xl'>
+                    <span className='text-green-700'>&lt;</span>
+                    <span> <a href="/">Passop</a> </span>
+                    <span className='text-green-700'>OP/&gt;</span>
+                </div>
                 <ul className='flex gap-4' >
                     <li><a className='hover:font-bold hover:border-b-2 hover:border-black' href="/">Home</a></li>
                     <li><a className='hover:font-bold hover:border-b-2 hover:border-black' href="#">About</a></li>
