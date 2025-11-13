a569448ce49e60045829cc8a0f3a9bc0d31937f7
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-13 20:29:51 +0530

    passop-passwordmanager

--- VIDEO130/passop/src/components/Manager.jsx
+++ VIDEO130/passop/src/components/Manager.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 
 function Manager() {
-  return (
-    <div>
-        <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
-        
-              
-    </div>
-  )
+    return (
+        <div>
+            <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
+
+            <div className=" mycontainer bg-slate-50">
+                <h1>PassOP</h1>
+                <p>Your own Passwork Manager</p>
+
+                <div className='text-white flex flex-col p-4' >
+                    <input className=' rounded-full ' type="text" />
+                    <div className='flex' >
+                        <input type="text" />
+                        <input type="text" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Manager
