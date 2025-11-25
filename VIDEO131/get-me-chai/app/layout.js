eaf5b69fa658531f2acc69e97ab430deaca4c33a
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-25 18:27:24 +0530

    push

--- VIDEO131/get-me-chai/app/layout.js
+++ VIDEO131/get-me-chai/app/layout.js
@@ -14,9 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
         <Navbar/>
 
-        <div className="min-h-[87vh] absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] " >
+        <div className="min-h-[87vh] " >
         {children}
         </div> 
         
