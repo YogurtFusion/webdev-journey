93c740a951861f3039994ea5144fa2ca288f6e0c
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-26 18:03:43 +0530

    push

--- VIDEO131/get-me-chai/app/page.js
+++ VIDEO131/get-me-chai/app/page.js
@@ -2,11 +2,32 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex justify-center flex-col items-center text-white h-[44vh]" >
-      <div className="font-bold text-3xl">Buy Me a Chai</div>
-      <p>
-        A crowdfunding platform for creators. Get funded by your fans and followers . Start now !
-      </p>
-    </div>
+    <>
+      <div className="flex justify-center flex-col items-center text-white h-[44vh] gap-4 ">
+        <div className="font-bold text-5xl flex justify-center items-center gap-2">Buy Me a Chai <span><img src="/tea.gif" width={88} alt="" /></span> </div>
+        <p>
+          A crowdfunding platform for creators. Get funded by your fans and
+          followers . Start now !
+        </p>
+        <div>
+          <button
+            type="button"
+            class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            Start Here
+          </button>
+
+          <button
+            type="button"
+            class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            Read More
+          </button>
+        </div>
+      </div>
+      <div className="bg-white h-1 opacity-10">
+
+      </div>
+    </>
   );
 }
