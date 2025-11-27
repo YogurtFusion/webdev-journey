fe0261158bab64f6744c937b2a4a94426b30204e
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-27 22:34:11 +0530

    push

--- VIDEO131/get-me-chai/app/[username]/page.js
+++ VIDEO131/get-me-chai/app/[username]/page.js
@@ -23,8 +23,50 @@ function Username({ params }) {
 
       <div className="info flex flex-col justify-center items-center my-24 gap-2 ">
         <div className="font-bold text-lg">@{params.username}</div>
-        <div>Creating Animated art for VTT'S</div>
-        <div>9718 member82 post$15, 450/release</div>
+        <div className="text-slate-400" >Creating Animated art for VTT'S</div>
+        <div className="text-slate-400" >9718member . 82post . 15,450/release</div>
+      </div>
+
+      <div className="payment flex gap-3 w-[80%] mt-11 ">
+        <div className="suppoters w-1/2 bg-slate-900 rounded-lg text-white p-10">
+        <h2 className="text-2xl font-bold my-5 " >Suppoters</h2>
+        <ul className="mx-5 text-lg">
+          <li className="my-4 flex gap-2 items-center"><img width={33} src="/avatar.gif" alt="" /><span>
+            shubham donated <span className="font-bold">$30</span> with a message "bro app mere sabase bare fan ho "  </span>
+            </li>
+          <li className="my-4 flex gap-2 items-center"><img width={33} src="/avatar.gif" alt="" /><span>
+            Rohan donated <span className="font-bold">$30</span> with a message "sir mera autograph loge ?  "  </span>
+            </li>
+          <li className="my-4 flex gap-2 items-center"><img width={33} src="/avatar.gif" alt="" /><span>
+            Rohan donated <span className="font-bold">$30</span> with a message "
+            sucess spits on your shoose "  </span>
+            </li>
+          <li className="my-4 flex gap-2 items-center"><img width={33} src="/avatar.gif" alt="" /><span>
+            Tarun donated <span className="font-bold">$30</span> with a message " bro why sucees hates you ? "  </span>
+            </li>
+        </ul>
+        </div>
+          <div className="makePayment w-1/2 bg-slate-900 rounded-lg text-white p-10 ">
+          <h2 className="text-2xl font-bold m-5 ">Make a Payment</h2>
+          <div className="flex gap-2 flex-col ">
+            {/* input for name and message */}
+            <div>
+              <input type="text" className="w-full p-3 rounded-lg bg-slate-800" placeholder="Enther your name" />
+            </div>
+              <input type="text" className="w-full p-3 rounded-lg bg-slate-800" placeholder="Enther your message" />
+              <input type="text" className="w-full p-3 rounded-lg bg-slate-800" placeholder="Enther your amount" />
+
+              <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Pay</button>
+            
+          </div>
+
+          <div className="flex gap-2 mt-5">
+            <button className="bg-slate-800 p-3 rounded-lg"> pay $10</button>
+            <button className="bg-slate-800 p-3 rounded-lg"> pay $20</button>
+            <button className="bg-slate-800 p-3 rounded-lg"> pay $30</button>
+          </div>
+          </div>
+
       </div>
     </>
   );
