82d85bb5567684e32f78232d6882538b88e8f6b4
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-27 15:33:37 +0530

    push

--- VIDEO131/get-me-chai/components/Navbar.js
+++ VIDEO131/get-me-chai/components/Navbar.js
@@ -5,15 +5,15 @@ import { useSession, signIn, signOut } from "next-auth/react";
 
 function Navbar() {
   const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email}
-        <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
+  // if (session) {
+  //   return (
+  //     <>
+  //       Signed in as {session.user.email}
+  //       <br />
+  //       <button onClick={() => signOut()}>Sign out</button>
+  //     </>
+  //   );
+  // }
 
   return (
     <nav className="bg-gray-950 text-white flex justify-between items-center px-4 h-16">
@@ -31,16 +31,38 @@ function Navbar() {
             </ul> */}
 
       <div>
+        {session && (
+          <Link href={"/dashboard"}>
+            <button
+              type="button"
+              className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            >
+              Dashboard
+            </button>
+          </Link>
+        )}
+        {session && (
+          <button
+            type="button"
+            className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            onClick={() => {
+              signOut();
+            }}
+          >
+            signOut
+          </button>
+        )}
 
-        <Link href={"/login"}>
-        <button
-          type="button"
-          className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-          Login
-        </button>
-        </Link>
-
-
+        {!session && (
+          <Link href={"/login"}>
+            <button
+              type="button"
+              className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            >
+              Login
+            </button>
+          </Link>
+        )}
       </div>
     </nav>
   );
