a1d1471ab267bcf3d1c05674249f9d0c274ebe4d
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-29 20:08:55 +0530

    push

--- VIDEO132/navigation/app/page.js
+++ VIDEO132/navigation/app/page.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { usePathname } from "next/navigation";
 
 export default function Home() {
  const pathname = usePathname()
---
+++ VIDEO132/navigation/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { usePathname } from "next/navigation";
+
+const Navbar = () => {
+  const pathname = usePathname();
+  return (
+    <div>
+      <div className="">
+        Navbar
+      </div>
+      <div className="">
+        this navbar is inside{pathname}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
