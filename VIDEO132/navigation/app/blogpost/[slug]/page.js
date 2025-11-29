5e109c5c0e7814e61f0b1a38f7186646540293a2
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-29 20:31:44 +0530

    push

--- VIDEO132/navigation/app/blogpost/[slug]/page.js
+++ VIDEO132/navigation/app/blogpost/[slug]/page.js
@@ -1,14 +1,25 @@
-"use client"
-import React from 'react'
-import { useParams } from 'next/navigation'
+"use client";
+import React from "react";
+import { useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 const page = () => {
-    const params = useParams()
+  const router = useRouter();
+  const params = useParams();
   return (
-    <div> <div className="">this is use params bitch and this is params 👉 {params.slug} </div>blogpage
-        
+    <div>
+      {" "}
+      <div className="">
+        this is use params bitch and this is params 👉 {params.slug}{" "}
+      </div>
+      blogpage
+      <div className="chal_chiaya_chaiya">
+        <button type="button" onClick={() => router.push("/dashboard")}>
+          Dashboard
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- VIDEO132/navigation/app/page.js
+++ VIDEO132/navigation/app/page.js
@@ -1,12 +1,14 @@
 "use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useSearchParams } from "next/navigation";
 
 
 export default function Home() {
+  const searchparams = useSearchParams()
   return (
     <>
-    Heyy this is main page
+    Heyy this is main page and blog is {searchparams.get("blog")} and utm source is{searchparams.get("utm_source")} 
     </>
   );
 }
