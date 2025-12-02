7843eca7202bfd7221ca58be4cbf5b572a5708dd
YogurtFusion <aniketkumar33805@outlook.com>
2025-12-03 00:23:07 +0530

    ^O^

--- VIDEO134/enviroment-variables/app/page.js
+++ VIDEO134/enviroment-variables/app/page.js
@@ -1,13 +1,16 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  console.log("the id is ", process.env.ID, "and the password is ", process.env.SECRET);
+  // console.log("the id is ", process.env.ID, "and the password is ", process.env.SECRET);
   
   return (
   <div className="">
-    hey this is a home page and here is ID: {process.env.ID}
-    <p>and the secret is: {process.env.SECRET} </p>
+    {/* hey this is a home page and here is ID: {process.env.ID} */}
+    {/* <p>and the secret is: {process.env.SECRET} </p> */}
+    <h2>the public id is {process.env.NEXT_PUBLIC_ID} </h2>
+    <h1>and the test is {process.env.NEXT_PUBLIC_TEST} </h1>
   </div>
   );
 }
