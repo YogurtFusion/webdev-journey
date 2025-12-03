f1512cbc31836d77a46540a3293c2def2cc68a4b
YogurtFusion <aniketkumar33805@outlook.com>
2025-12-04 00:47:11 +0530

    C:/Program Files/Git/≧▽≦)/

---
+++ VIDEO135/style_nextjs/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import style from "@/style/home.module.css"
+export default function Home() {
+  return (
+    <div className={style.red}>
+      he this is red
+    </div>
+  );
+}
