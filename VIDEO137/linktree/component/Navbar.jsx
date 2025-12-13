17f1e5ca0dec8de00a8e34c8a9dd7330d7c37baf
YogurtFusion <aniketkumar33805@outlook.com>
2025-12-13 21:55:07 +0530

    (C:/Program Files/Git/≧▽≦)/

--- VIDEO137/linktree/component/Navbar.jsx
+++ VIDEO137/linktree/component/Navbar.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <nav>
-        
+    <nav className=" flex justify-between items-center bg-white w-[80vw] absolute top-10 right-[10vw] rounded-full p-5 ">
+      <div className="logo flex gap-20 items-center">
+        <img 
+        className=" h-8"
+          src="https://cdn.prod.website-files.com/666255f7f2126f4e8cec6f8f/66634daccb34e6d65a41c76d_download.svg"
+          alt="linktree-vg"
+        />
+        <ul className="flex gap-10 items-center" >
+          <li> products </li>
+          <li> Templates</li>
+          <li> Marketplace</li>
+          <li> learn</li>
+          <li> Pricing</li>
+        </ul>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
