e38e6dc01cf7672bde566422eb788706a250a960
YogurtFusion <aniketkumar33805@outlook.com>
2025-12-14 20:09:22 +0530

    (C:/Program Files/Git/≧▽≦)/

--- VIDEO137/linktree/app/page.js
+++ VIDEO137/linktree/app/page.js
@@ -1,11 +1,43 @@
+"use client"
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [invalue, setInvalue] = useState("linktr.ee/");
   return (
     <main>
-      <section className="bg-[#d2e823] min-h-screen">
-       
+      <section className="bg-[#d2e823] min-h-screen grid grid-cols-2 ">
+        <div className="flex justify-center flex-col ml-[10vw] gap-3 ">
+          <p className="text-[#254f1a] font-bold text-7xl">
+            A link in bio built{" "}
+          </p>
+          <p className="text-[#254f1a] font-bold text-7xl">for you. </p>
+
+          <p className="text-[#254f1a] text-xl my-4">
+            join 70M+ people using Linktree for their link in bio. One link to
+            help you share everything you create, curate and sell from your
+            Instagram, TikTok, Twitter, YouTube and other social media profiles.
+          </p>
+
+          <div className="input flex gap-2">
+            <input
+              type="text"
+              className="p-2 focus:outline-green-700 bg-white rounded-md "
+              placeholder="bittr.ee/your-url"
+              onChange={(e) => setInvalue(e.target.value)}
+              value={invalue}
+            />
+            <button className="bg-pink-200 rounded-full px-4 py-4 font-semibold">
+              Claim your Bittree
+            </button>
+          </div>
+        </div>
+        <div className="flex items-center justify-center flex-col my-[10vw] mt-[10vw] ">
+          <img src="/home.png" alt="homepage image" />
+        </div>
       </section>
+
+      <section className="bg-red-700 min-h-screen"></section>
     </main>
   );
 }
--- VIDEO137/linktree/component/Navbar.jsx
+++ VIDEO137/linktree/component/Navbar.jsx
@@ -2,21 +2,26 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className=" flex justify-between items-center bg-white w-[80vw] absolute top-10 right-[10vw] rounded-full p-5 ">
-      <div className="logo flex gap-20 items-center">
-        <img 
-        className=" h-8"
+    <nav className=" flex justify-between items-center bg-white w-[80vw]  fixed top-10  mx-auto right-0 left-0 rounded-full py-5  px-7  ">
+      <div className="logo flex gap-20 justify-between items-center ">
+        <img
+          className=" h-8"
           src="https://cdn.prod.website-files.com/666255f7f2126f4e8cec6f8f/66634daccb34e6d65a41c76d_download.svg"
           alt="linktree-vg"
         />
-        <ul className="flex gap-10 items-center" >
+        <ul className="flex gap-10 items-center">
           <li> products </li>
           <li> Templates</li>
           <li> Marketplace</li>
           <li> learn</li>
           <li> Pricing</li>
         </ul>
+      
       </div>
+      <div className="flex gap-10 font-bold ">
+          <button className="login hover:bg-gray-200 bg-gray-100 p-4 rounded-lg ">Log in </button>
+          <button className="signup bg-gray-900 text-white p-4 rounded-full"> Sign up free</button>
+        </div>
     </nav>
   );
 };
