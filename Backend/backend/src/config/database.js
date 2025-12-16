d6d5e7d9aa95692533b3d8b8ea88a095d4b95f8b
YogurtFusion <aniketkumar33805@outlook.com>
2025-12-16 17:45:18 +0530

    theory of sigma web dev officially completed

--- Backend/backend/src/config/database.js
+++ Backend/backend/src/config/database.js
@@ -1,17 +1,16 @@
 import mongoose from "mongoose";
 
-const connectDB = async () =>{
-    try {
-        const connectionInstance = await mongoose.connect
-        (`${process.env.MONGODB_URI}`)
-        console.log(`\n data base initiated✅
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}`
+    );
+    console.log(`\n data base initiated✅
             ${connectionInstance.connection.host}`);
-        
-    } catch (error) {
-        console.log(`connection falied to MONGO DB mongoose is crying`, error);
-        process.exit(1)
-        
-    }
-}
+  } catch (error) {
+    console.log(`connection falied to MONGO DB mongoose is crying`, error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
