4b940aa0b40ef28c48436c309182f2d297c79a28
YogurtFusion <aniketkumar33805@outlook.com>
2025-12-23 15:52:34 +0530

    get post completed

--- Backend/backend/src/controllers/post.controller.js
+++ Backend/backend/src/controllers/post.controller.js
@@ -2,26 +2,50 @@ import { Post } from "../models/post.model.js";
 
 //Create post
 
-const createPost = async(req, res)=>{
-    try{
-        const {name, description, age} = req.body;
+const createPost = async (req, res) => {
+  try {
+    const { name, description, age } = req.body;
 
-        if(!name||!description||!age){
-            return res.status(400).json({
-                message: "All feild are required"
-            });
-            }
-        const post = await Post.create({name, description, age})
-        
-        res.status(200).json({
-            message: "Post created successfully", post
-        })
-        
-    } catch (error) {
-        res.status(500).json({
-            message: "internal server",error
-        })
+    if (!name || !description || !age) {
+      return res.status(400).json({
+        message: "All feild are required",
+      });
+    }
+    const post = await Post.create({ name, description, age });
+
+    res.status(200).json({
+      message: "Post created successfully",
+      post,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "internal server",
+      error,
+    });
+  }
+};
+
+// get posts 
+
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error,
+    });
+  }
+};
+
+// U = update 
+
+const updatePost = async(req, res)=>{
+    try{
+// basic validation to check if body is empty
+    }catch(error){
 
     }
 }
-export {createPost}
+export { createPost, getPosts };
--- Backend/backend/src/routes/post.routes.js
+++ Backend/backend/src/routes/post.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getPosts } from "../controllers/post.controller.js";
 
 const router = Router()
 
 router.route("/create").post(createPost)
+router.route("/getPosts").get(getPosts)
 export default router
